d5d75a2c9ae917818758e5bcfe47302343ab4efc
Wanpeng <wanpengliu@gmail.com>
2016-07-15 13:52:54 +0100

    update project test

--- src/main/java/com/news/feed/BadNewPoliceApplication.java
+++ src/main/java/com/news/feed/BadNewPoliceApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BadNewPoliceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Hello Harish & Zhao");
 		SpringApplication.run(BadNewPoliceApplication.class, args);
 	}
 }
