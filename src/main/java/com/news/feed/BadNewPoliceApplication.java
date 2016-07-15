a87865675206f7701cefdc3579629ea20fe65dbe
Wanpeng <wanpengliu@gmail.com>
2016-07-15 13:49:27 +0100

    Initial commit

---
+++ src/main/java/com/news/feed/BadNewPoliceApplication.java
@@ -0,0 +1,12 @@
+package com.news.feed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BadNewPoliceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BadNewPoliceApplication.class, args);
+	}
+}
